a23fa8350ade59dc950981ff72356480bf00cb9f
Roman Nikitenko <rnikiten@redhat.com>
2023-04-29 09:11:55 +0300

    Add check if file exists
    Signed-off-by: Roman Nikitenko <rnikiten@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,23 @@
 import * as vscode from "vscode";
+import * as fs from "fs";
 
 export function activate(context: vscode.ExtensionContext) {
   const outputChannel = vscode.window.createOutputChannel('Custom Open URL');
 
   const disposable = vscode.commands.registerCommand('custom-open-command', (arg: string) => {
     try {
+      outputChannel.clear();
       outputChannel.appendLine(`Custom open command has got the arg: ${arg}`);
       outputChannel.show();
 
-      vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(arg));
+      const url = vscode.Uri.parse(arg);
+      if (!fs.existsSync(url.path)) {
+        outputChannel.appendLine(`The file was not found by path: ${url.path}, maybe it is a web url`);
+        outputChannel.show();
+        // do not return, because we want to open the url anyway (maybe it is a web url) 
+      }
+
+      vscode.commands.executeCommand('vscode.open', url);
     } catch (error) {
       if (error instanceof Error) {
         outputChannel.appendLine(error.message);
